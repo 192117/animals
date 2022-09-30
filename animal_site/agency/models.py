32b3bba74430118eb0a2959d12680e0cb85776e3
arturkalimullin <noti1996@gmail.com>
2022-09-30 07:54:03 +0300

    Добавление моделей.

--- animal_site/agency/models.py
+++ animal_site/agency/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+
+class OrderPhoto(models.Model):
+    CHOICES = {
+        'Success': 'Выполнено',
+        'Confirmation': 'Подтверждение',
+        'Awaiting': 'Ожидание подтверждения',
+        'Cancel': 'Отмена',
+        'Archive': 'Архив'
+    }
+    email = models.EmailField(
+        verbose_name='Электронная почта заявителя',
+        help_text="Введите вашу почту для заявки",
+    )
+    phone_number = PhoneNumberField(
+        verbose_name="Мобильный телефон заявителя",
+        help_text="Введите ваш мобильный телефон для связи",
+    )
+    last_name = models.CharField(
+        verbose_name="Фамилия заявителя",
+        help_text="Введите вашу фамилию",
+        max_length=30,
+    )
+    first_name = models.CharField(
+        verbose_name="Имя заявителя",
+        help_text="Введите ваше имя",
+        max_length=30,
+    )
+    body = models.TextField(
+        verbose_name="Описание заявки",
+        help_text="Введите ваше пожелание",
+    )
+    status = models.CharField(
+        verbose_name='Статус заявки',
+        choices=CHOICES,
+        default='Awaiting',
+        max_length=20
+    )
+
+
+class AddModelAnimal(models.Model):
+    pass
+
+class Animals(models.Model):
+    pass
+
+
